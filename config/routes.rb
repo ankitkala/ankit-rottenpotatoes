8ae4068da2387340206dd30e929a136808856625
Ankit Kala <ankit8364@gmail.com>
2012-06-15 16:00:44 +0530

    new start

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
-  match "/movies/:id" => "movies#index"
 
   # Sample resource route with options:
   #   resources :products do
